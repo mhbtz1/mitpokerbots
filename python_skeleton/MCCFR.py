79c202e87c61f698bb4be519857bfb3ab5d86fca
Matthew Habtezgi <mtarekegn143@gmail.com>
2022-01-14 14:49:00 -0500

    updated strategy table

--- python_skeleton/MCCFR.py
+++ python_skeleton/MCCFR.py
@@ -92,7 +92,7 @@ class CFR_Game_State:
         self.p2_stack = p2_stack
         self.pot_size = pot_size
         self.IP_PLAYER = IP_PLAYER
-        self.ACTIONS_PER_STREET = 0
+        self.cur_street = []
 
         self.deck = []
         for s in CFR_Game_State.cards:
@@ -120,7 +120,7 @@ class CFR_Game_State:
             return False
         if(history[-3:] == 'FLD'):
             return True
-        elif(len(self.board_cards)==5 and len(history)>=27): #we only allow 2 actions per street, and we fully solve preflop using precomputed range charts
+        elif(len(self.board_cards)==5): #we only allow 2 actions per street, and we fully solve preflop using precomputed range charts
             return True
         else:
             return False
@@ -152,22 +152,22 @@ class CFR_Game_State:
     def apply_action(self, history, action_string, player_num):
        #print("HISTORY: {}, ACTION: {}".format(history,action_string))
        if(action_string[0]=='R'):#raise
-           self.ACTIONS_PER_STREET += 1
            relsize = 2 if action_string[1:]=='CC' else float(int(action_string[1:]))/100
            raiseamt = relsize*self.pot_size
+           self.cur_street.append(action_string)
            self.pot_size += raiseamt
            if(player_num==0):
                self.p1_stack -= raiseamt
            else:
                self.p2_stack -= raiseamt
        elif(action_string=='CHK'):#check
-           self.ACTIONS_PER_STREET += 1
+           self.cur_street.append('CHK')
        elif(action_string=='CAL'): #call a raise
-           self.ACTIONS_PER_STREET += 1
            last_action = '' if len(history) < 3 else history[-3:]
            if(last_action != ''):
                conv_bet = 2 if last_action[1:] == 'CC' else float(int(last_action[1:])) / 100
                original_bet_size = (conv_bet)/float(1+conv_bet) * self.pot_size
+               self.cur_street.append('CAL')
                if(player_num == 0):
                    self.p1_stack -= original_bet_size
                    self.pot_size += original_bet_size
@@ -180,105 +180,71 @@ class CFR_Game_State:
     def get_all_actions(self, history, player_num):
         '''
         Default ordering is: R66, R75, R80, R200, CAL, CHK, FLD
+        Gets all the current actions that we can take from our state
         '''
-
-
         default_actions = ['R66', 'R75', 'R80', 'RCC', 'CAL', 'CHK', 'FLD']
         last_action = '' if len(history) < 3 else history[-3:]
+        print("THE CURRENT STREET: {}".format(self.cur_street))
 
-        if(self.ACTIONS_PER_STREET == CFR_Game_State.MAX_ACTIONS_PER_STREET):
-            self.ACTIONS_PER_STREET = 0
-            #print("ADVANCED BOARD")
-            if (len(self.board_cards) < 5):
-                self.board_cards += [self.deck[0]]
-                del self.deck[0]
-            for i in range(len(default_actions)-1,-1,-1):
-                if(default_actions[i][0]=='R'):
-                    del default_actions[i]
-        if(last_action=='CAL'):
-            self.ACTIONS_PER_STREET = 0
-            #print("ADVANCED BOARD")
-            if (len(self.board_cards) < 5):
-                self.board_cards += [self.deck[0]]
-                del self.deck[0]
-            if('CAL' in default_actions):
-                del default_actions[default_actions.index('CAL')]
-            if('FLD' in default_actions):
-                del default_actions[default_actions.index('FLD')]
-
-
-        '''
-        remove actions based upon the bet sizes our stack allows us to do
-        '''
-        for i in range(len(default_actions)-1,-1,-1):
-            action = default_actions[i]
-            if(action == 'CHK' or action == 'CAL' or action == 'FLD'):
-                continue
-
-            sizing = 2 if action[1:]=='CC' else float(int(action[1:]))/100
-            truesize = sizing * self.pot_size
-            if(player_num == 0):
-                if(truesize > self.p2_stack):
-                    del default_actions[i]
-            else:
-                if(truesize > self.p1_stack):
-                    del default_actions[i]
-
-        '''
-        remove actions based on the last action
-        '''
-        if(last_action != ''):
+        if(len(self.cur_street) == CFR_Game_State.MAX_ACTIONS_PER_STREET):
+            self.board_cards += [self.deck[0]]
+            del self.deck[0]
+            print("UPDATE BOARD")
             if(last_action[0]=='R'):
-                #if the last action is a raise, we can either reraise, call, or fold
-                del default_actions[default_actions.index('CHK')]
-                for i in range(len(default_actions)-1,-1,-1):
-                    if(default_actions[i] == 'FLD'):
-                        continue
-                    if(default_actions[i] == 'CHK'):
-                        del default_actions[i]
-                    if(default_actions[i][0] == 'R'):
-                        amt = 2 if default_actions[i][1:]=='CC' else float(int(default_actions[i][1:]))/100
-                        betsize = amt * self.pot_size
-                        lamt = 2 if last_action[1:]=='CC' else float(int(last_action[1:]))/100
-                        lsize = (float(lamt)/(1 + lamt)) * self.pot_size
-                        #print("CURRENT BET SIZE: {}".format(lsize), 'FUTURE BET SIZE: {}'.format(betsize))
-                        if(player_num==0):
-                            if(betsize > self.p2_stack or lsize * 2 > betsize):
-                                del default_actions[i]
-                        elif(player_num==1):
-                            if(betsize > self.p1_stack or lsize * 2 > betsize):
-                                del default_actions[i]
-                    if(default_actions[i]== 'CAL'):
-                        conv_bet = 2 if last_action[1:]=='CC' else float(int(last_action[1:]))/100
-                        original_bet_size = (conv_bet)/float(1+conv_bet) * self.pot_size
-                        if(player_num==0):
-                            if(original_bet_size > self.p2_stack):
-                                del default_actions[i]
-                        else:
-                            if(original_bet_size > self.p1_stack):
-                                del default_actions[i]
-            elif(last_action=='CHK'):
-                if('FLD' in default_actions):
-                    del default_actions[default_actions.index('FLD')]
-                if('CAL' in default_actions):
-                    del default_actions[default_actions.index('CAL')]
-                #if the last action is a check, then we can bet or check back
-            elif(last_action=='CAL'):
-                if('FLD' in default_actions):
-                    del default_actions[default_actions.index('FLD')]
-                if('CAL' in default_actions):
-                    del default_actions[default_actions.index('CAL')]
-                self.ACTIONS_PER_STREET = 0
-                if(len(self.board_cards)<5):
-                    self.board_cards += [self.deck[0]]
-                    del self.deck[0]
-                #if the last action is a call, then there is nothing else to do
+                self.cur_street.clear()
+                return ['CAL','FLD']
+            else:
+                self.cur_street.clear()
+                return ['CHK', 'FLD']
         else:
             for i in range(len(default_actions)-1,-1,-1):
-                if(default_actions[i][0]=='C' and default_actions[i] != 'CHK'):
-                    del default_actions[i]
-        #print("ANSWER: {}".format(default_actions))
+                if(last_action==''):
+                    if (default_actions[i] == 'CAL'):
+                        del default_actions[i]
+                    if(default_actions[i][0]=='R'):
+                        sizing = 2 if default_actions[i][1:] == 'CC' else float(default_actions[i][1:]) / 100
+                        betsize = sizing * self.pot_size
+                        our_stack = self.p1_stack if player_num == 0 else self.p2_stack
+                        if (betsize > our_stack):
+                            del default_actions[i]
+                else:
+                    if(last_action[0]=='R'):
+                        if(default_actions[i]=='CHK'):
+                            del default_actions[i]
+                        if(default_actions[i]=='CAL'):
+                            sizing = 2 if last_action[1:]=='CC' else float(last_action[1:])/100
+                            betsize = (float(sizing)/(sizing+1))*self.pot_size
+                            our_stack = self.p1_stack if player_num==0 else self.p2_stack
+                            if(betsize > our_stack):
+                                del default_actions[i]
+                        elif(default_actions[i][0]=='R' and len(self.cur_street)<=2):#force the bot to not spam reraise (although it still is able to)
+                            sizing = 2 if default_actions[i][1:]=='CC' else float(default_actions[i][1:])/100
+                            betsize = sizing * self.pot_size
+                            our_stack = self.p1_stack if player_num==0 else self.p2_stack
+                            if(betsize > our_stack):
+                                del default_actions[i]
+                    elif(last_action=='CAL'):
+                        if(default_actions[i]=='FLD' or default_actions[i] == 'CAL'):
+                            del default_actions[i]
+                        elif(default_actions[i][0]=='R'):
+                            sizing = 2 if default_actions[i][1:] == 'CC' else float(default_actions[i][1:]) / 100
+                            betsize = sizing * self.pot_size
+                            our_stack = self.p1_stack if player_num == 0 else self.p2_stack
+                            if (betsize > our_stack):
+                                del default_actions[i]
+                    elif(last_action=='CHK'):
+                        if(default_actions[i]=='CAL'):
+                            del default_actions[i]
+                        if (default_actions[i][0] == 'R' and len(self.cur_street) <= 1):
+                            sizing = 2 if default_actions[i][1:] == 'CC' else float(default_actions[i][1:]) / 100
+                            betsize = sizing * self.pot_size
+                            our_stack = self.p1_stack if player_num == 0 else self.p2_stack
+                            if (betsize > our_stack):
+                                del default_actions[i]
+
         return default_actions
+
+
 
 
 
@@ -315,8 +281,13 @@ class MCCFR():
             cur_game_state.apply_action(history_state[0:-3],history_state[-3:],cur_player)
         all_actions = cur_game_state.get_all_actions(history_state, (cur_player+1)%2)
 
+        do_not_raise = False
+        if (len(cur_game_state.cur_street) == 2 and cur_game_state.cur_street[0][0] == 'R' and
+                cur_game_state.cur_street[1][0] == 'R'):
+            do_not_raise = True
+            cur_game_state.cur_street.clear()
+
         #print("HISTORY: {}".format(history_state))
-        print("BOARD STATE: {}".format(cur_game_state.board_cards))
         '''
         print("STACKS: {}, {}".format(cur_game_state.p1_stack, game_state.p2_stack))
         print("POT: {}".format(cur_game_state.pot_size))
@@ -327,20 +298,26 @@ class MCCFR():
             print("TERMINAL STATE REACHED")
             utility = SwapPoker.SwapPoker.utility(cur_game_state,history_state,cur_player)
             print("UTILITY: {}".format(utility))
+            print("POT SIZE: {}".format(cur_game_state.pot_size))
+            print("STACK SIZES: {}, {}".format(cur_game_state.p1_stack,cur_game_state.p2_stack))
             return utility
 
         #add code for if we have a chance node here, and apply chance sampling
 
-
+        print('---------------------------------------------------------------------------------------')
         cur_hand = cur_game_state.p1_hole if cur_player == 0 else cur_game_state.p2_hole
         concat = ''.join(cur_hand)
         info_set = self.get_information_set(concat + history_state)
         cur_strategy = info_set.getStrategy(reach_probabilities[cur_player])
         cfr_vals = np.zeros(info_set.num_actions)
         print("ALL ACTIONS: {}".format(all_actions))
-
         parsed_list = self.parse_history(history_state)
         print("PARSED LIST: {}".format(parsed_list))
+        print("HISTORY STATE: {}".format(history_state))
+        print("BOARD STATE: {}".format(cur_game_state.board_cards))
+        print("CURRENT STREET: {}".format(cur_game_state.cur_street))
+        print('------------------------------------------------------------------------------------------')
+
         no_more_raise = False
         last_check = False
         last_call = False
@@ -350,25 +327,26 @@ class MCCFR():
             last_check = True
         if(len(parsed_list)>0 and parsed_list[-1]=='CAL'):
             last_call = True
-        for i, val in enumerate(all_actions):
 
-            if(last_check):
-                if(val == 'CAL' or val == 'FLD'):
+        string_to_index = {}
+        string_to_index['R66']=0
+        string_to_index['R75']=1
+        string_to_index['R80']=2
+        string_to_index['RCC']=3
+        string_to_index['CAL']=4
+        string_to_index['CHK']=5
+        string_to_index['FLD']=6
+
+
+        for val in (all_actions):
+            if(do_not_raise):
+                if(val[0]=='R'):
                     continue
-
-            if(no_more_raise):
-                if(val == 'CAL' or val == 'FLD'):
-                    action_prob = cur_strategy[i]
-                    nxt_reach = reach_probabilities.copy()
-                    nxt_reach[cur_player] *= action_prob
-                    history_state += val
-                    cfr_vals[i] = -self.mc_cfrm(cur_game_state, history_state, nxt_reach, (cur_player + 1) % 2)
-            else:
-                action_prob = cur_strategy[i]
-                nxt_reach = reach_probabilities.copy()
-                nxt_reach[cur_player] *= action_prob
-                history_state += val
-                cfr_vals[i] = -self.mc_cfrm(cur_game_state, history_state, nxt_reach, (cur_player + 1) % 2)
+            action_prob = cur_strategy[string_to_index[val]]
+            nxt_reach = reach_probabilities.copy()
+            nxt_reach[cur_player] *= action_prob
+            history_state += val
+            cfr_vals[string_to_index[val]] = -self.mc_cfrm(cur_game_state, history_state, nxt_reach, (cur_player + 1) % 2)
 
 
         node_value = cfr_vals.dot(cur_strategy)
@@ -385,7 +363,7 @@ class MCCFR():
 '''
 cfr_run = MCCFR()
 util = 0
-for i in range(250):
+for i in range(50):
     print('----------------------------------------------------------------------------------------------------------')
     reach_prob = np.ones(2)
     ip_player = 1 if random.uniform(0,1) <= 0.5 else 0
@@ -399,40 +377,39 @@ print("UTILITY: {}".format(util))
 
 
 
+
 info_sets = cfr_run.info_map
 for (pos, strat) in info_sets.items():
     rep_strat = [float(s) for s in strat.strategy]
     print(pos,rep_strat)
 '''
 
-
 #print(len(info_sets.keys()))
 
 
-cfr_test_run = CFR_Game_State(['Ac', '5s', 'Qd'], ['Qs', '9s'], 2000, ['Kc', '9c'], 2000, 0, 25)
+#cfr_test_run = CFR_Game_State(['Ac', '5s', 'Qd'], ['Qs', '9s'], 2000, ['Kc', '9c'], 2000, 0, 25)
+#print(cfr_test_run.get_all_actions('CAL',0))
+
 '''
-print(cfr_test_run.get_all_actions('',0))
+print(cfr_test_run.board_cards)
+cfr_test_run.get_all_actions('',1)
 cfr_test_run.apply_action('', 'RCC', 1)
-print('--------------------------------------------------------')
-print("POT SIZE: {}".format(cfr_test_run.pot_size))
-print("STACK SIZES: {}, {}".format(cfr_test_run.p1_stack, cfr_test_run.p2_stack))
-print("NUMBER OF ACTIONS: {}".format(cfr_test_run.ACTIONS_PER_STREET))
+print(cfr_test_run.get_all_actions('RCC',0))
+print('-----------------------------------------------------------')
 print(cfr_test_run.board_cards)
-print(cfr_test_run.p1_stack, cfr_test_run.p2_stack)
-print(cfr_test_run.get_all_actions('RCC', 0))
-cfr_test_run.apply_action('RCC', 'RCC', 0)
-print('--------------------------------------------------------')
-print("POT SIZE: {}".format(cfr_test_run.pot_size))
-print("STACK SIZES: {}, {}".format(cfr_test_run.p1_stack, cfr_test_run.p2_stack))
-print("NUMBER OF ACTIONS: {}".format(cfr_test_run.ACTIONS_PER_STREET))
+cfr_test_run.apply_action('RCC','RCC',0)
+print(cfr_test_run.get_all_actions('RCCRCC',1))
+print("----------------------------------------------------------")
 print(cfr_test_run.board_cards)
-print(cfr_test_run.p1_stack, cfr_test_run.p2_stack)
-cfr_test_run.get_all_actions('RCCRCC',1)
+cfr_test_run.apply_action('RCCRCC', 'CAL', 1)
+print(cfr_test_run.get_all_actions('RCCRCCCAL',0))
+print(cfr_test_run.board_cards)
+
+
 cfr_test_run.apply_action('RCCRCCC', 'CAL', 1)
 print("--------------------------------------------------------------")
 print("POT SIZE: {}".format(cfr_test_run.pot_size))
 print("STACK SIZES: {}, {}".format(cfr_test_run.p1_stack, cfr_test_run.p2_stack))
-print("NUMBER OF ACTIONS: {}".format(cfr_test_run.ACTIONS_PER_STREET))
 print(cfr_test_run.board_cards)
 print(cfr_test_run.p1_stack, cfr_test_run.p2_stack)
 cfr_test_run.get_all_actions('RCCRCC',0)
@@ -440,7 +417,6 @@ cfr_test_run.apply_action('RCCCRCCCAL','RCC',0)
 print('--------------------------------------------------------')
 print("POT SIZE: {}".format(cfr_test_run.pot_size))
 print("STACK SIZES: {}, {}".format(cfr_test_run.p1_stack, cfr_test_run.p2_stack))
-print("NUMBER OF ACTIONS: {}".format(cfr_test_run.ACTIONS_PER_STREET))
 print(cfr_test_run.board_cards)
 print(cfr_test_run.p1_stack, cfr_test_run.p2_stack)
 cfr_test_run.get_all_actions('RCCRCCRCCRCC',1)
@@ -448,11 +424,11 @@ cfr_test_run.apply_action('RCCCRCCRCCRCC','CAL',1)
 print('--------------------------------------------------------')
 print("POT SIZE: {}".format(cfr_test_run.pot_size))
 print("STACK SIZES: {}, {}".format(cfr_test_run.p1_stack, cfr_test_run.p2_stack))
-print("STACK SIZES: {}, {}".format(cfr_test_run.p1_stack, cfr_test_run.p2_stack))
 print(cfr_test_run.board_cards)
 print(cfr_test_run.p1_stack, cfr_test_run.p2_stack)
 cfr_test_run.get_all_actions('RCCRCCRCCRCCCAL',1)
 '''
+
 
 #cfr_test_run.ACTIONS_PER_STREET = 2
 #cfr_test_run.get_all_actions('RCCRCC', 0)
@@ -524,7 +500,7 @@ def train_optimised(num_iterations):
 
 
 
-#train_optimised(200)
+train_optimised(200)
 
 
 
